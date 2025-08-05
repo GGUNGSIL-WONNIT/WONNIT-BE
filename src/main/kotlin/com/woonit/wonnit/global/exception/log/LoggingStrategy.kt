8ebb92003ad69d835f1962b528cd781426ab3626
saeyeonn <sy01lim@naver.com>
2025-08-05 17:53:06 +0900

    feat: 예외 로깅 전략 및 로그 레벨 Enum 정의
    - DefaultLoggingStrategy 및 LoggingStrategy 인터페이스 구현
    - 예외 유형에 따른 로그 레벨을 분리하여 유연한 예외 로깅 처리 가능

---
+++ src/main/kotlin/com/woonit/wonnit/global/exception/log/LoggingStrategy.kt
@@ -0,0 +1,10 @@
+package com.woonit.wonnit.global.exception.log
+
+import com.woonit.wonnit.global.exception.code.ErrorCode
+import com.woonit.wonnit.global.exception.base.BaseException
+import jakarta.servlet.http.HttpServletRequest
+
+interface LoggingStrategy {
+    fun log(exception: BaseException, request: HttpServletRequest)
+    fun log(exception: Exception, request: HttpServletRequest, errorCode: ErrorCode)
+}
