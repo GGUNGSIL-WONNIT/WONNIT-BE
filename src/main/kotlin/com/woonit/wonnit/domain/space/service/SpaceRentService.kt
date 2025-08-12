cd260ee0b7a2518577d333b63d096ed5448370d7
maruhxn <maruhan1016@gmail.com>
2025-08-13 02:43:46 +0900

    feat: 공간 대여 및 반납 플로우 기능 구현

---
+++ src/main/kotlin/com/woonit/wonnit/domain/space/service/SpaceRentService.kt
@@ -0,0 +1,48 @@
+package com.woonit.wonnit.domain.space.service
+
+import com.woonit.wonnit.domain.space.Space
+import com.woonit.wonnit.domain.space.repository.SpaceRepository
+import com.woonit.wonnit.domain.user.User
+import com.woonit.wonnit.domain.user.repository.UserRepository
+import jakarta.transaction.Transactional
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+@Transactional
+class SpaceRentService(
+    val spaceRepository: SpaceRepository,
+    val userRepository: UserRepository,
+) {
+
+    fun rent(userId: String, spaceId: String) {
+        val space = getSpace(spaceId)
+        val renter = getUser(userId)
+        space.rent(renter)
+    }
+
+    fun returnRequest(userId: String, spaceId: String) {
+        val space = getSpace(spaceId)
+        val renter = getUser(userId)
+        space.returnRequest(renter)
+    }
+
+    fun returnReject(userId: String, spaceId: String) {
+        val space = getSpace(spaceId)
+        val renter = getUser(userId)
+        space.returnReject(renter)
+    }
+
+    fun returnApprove(userId: String, spaceId: String) {
+        val space = getSpace(spaceId)
+        val renter = getUser(userId)
+        space.returnApprove(renter)
+    }
+
+    private fun getUser(userId: String): User = (userRepository.findByIdOrNull(UUID.fromString(userId))
+        ?: throw NoSuchElementException("유저 정보를 찾을 수 없습니다"))
+
+    private fun getSpace(spaceId: String): Space = spaceRepository.findByIdOrNull(UUID.fromString(spaceId))
+        ?: throw NoSuchElementException("공간 정보를 찾을 수 없습니다")
+}
