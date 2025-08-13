6ccdccebf088e4ce0b4d1576bcec99b8f4fda0c9
saeyeonn <sy01lim@naver.com>
2025-08-13 17:34:35 +0900

    refactor(space): 생성자 val 프로퍼티를 var + protected set으로 리팩토링
    - Space 엔티티의 주 생성자에 선언된 val 프로퍼티들을 클래스 본문으로 이동
    - 각 프로퍼티에 var + protected set 적용하여 외부 수정은 제한하고 내부 수정 가능하게 변경
    - JPA 매핑 어노테이션(@Column, @Embedded, @ElementCollection, @ManyToOne) 유지 및 위치 조정
    - 연관관계 필드(user)는 읽기 전용(val)로 두어 외부에서 변경 불가 처리

--- src/main/kotlin/com/woonit/wonnit/domain/space/Space.kt
+++ src/main/kotlin/com/woonit/wonnit/domain/space/Space.kt
@@ -6,54 +6,98 @@ import com.woonit.wonnit.domain.share.OperationalInfo
 import com.woonit.wonnit.domain.share.PhoneNumber
 import com.woonit.wonnit.domain.user.User
 import com.woonit.wonnit.global.entity.BaseEntity
-import com.woonit.wonnit.global.entity.PrimaryKeyEntity
 import jakarta.persistence.*
+import lombok.NoArgsConstructor
+import kotlin.reflect.KProperty
 
 @Entity
 @Table(name = "spaces")
-class Space(
+@NoArgsConstructor
+class Space (
+    name: String,
+    addressInfo: AddressInfo,
+    mainImgUrl: String,
+    subImgUrls: MutableList<String>,
+    spaceCategory: SpaceCategory,
+    size: Double,
+    operationalInfo: OperationalInfo,
+    amountInfo: AmountInfo,
+    spaceModelUrl: String?,
+    modelThumbnailUrl: String?,
+    phoneNumber: PhoneNumber,
+    precautions: String?,
+    tags: MutableList<String>,
+    user: User,
+): BaseEntity () {
 
     @Column(length = 255, nullable = false)
-    val name: String,
+    var name: String = name
+        protected set
+
+    @Embedded
+    var addressInfo: AddressInfo = addressInfo
+        protected set
 
     @Column(nullable = false)
-    val addressInfo: AddressInfo,
+    var mainImgUrl: String = mainImgUrl
+        protected set
 
-    @Column(nullable = false)
-    val mainImgUrl: String,
+    @ElementCollection
+    @CollectionTable(
+        name = "space_sub_images",
+        joinColumns = [JoinColumn(name = "space_id")]
+    )
+    @Column(name = "url", nullable = false)
+    var subImgUrls: MutableList<String> = subImgUrls
+        protected set
 
-    val subImgUrls: List<String>,
-
-    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    val spaceCategory: SpaceCategory,
+    @Column(nullable = false)
+    var spaceCategory: SpaceCategory = spaceCategory
+        protected set
 
     @Column(nullable = false)
-    val size: Double,
+    var size: Double = size
+        protected set
 
-    val operationalInfo: OperationalInfo,
+    @Embedded
+    var operationalInfo: OperationalInfo = operationalInfo
+        protected set
 
-    val amountInfo: AmountInfo,
+    @Embedded
+    var amountInfo: AmountInfo = amountInfo
+        protected set
 
-    val spaceModelUrl: String?,
+    var spaceModelUrl: String? = spaceModelUrl
+        protected set
+
+    var modelThumbnailUrl: String? = modelThumbnailUrl
+        protected set
 
     @Embedded
     @AttributeOverride(
         name = "value",
         column = Column(name = "phone_number", nullable = false)
     )
-    val phoneNumber: PhoneNumber,
+    var phoneNumber: PhoneNumber = phoneNumber
+        protected set
 
     @Column(columnDefinition = "TEXT")
-    val precautions: String?,
-
-    val tags: MutableList<String> = mutableListOf(),
+    var precautions: String? = precautions
+        protected set
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
-    val user: User
+    val user: User = user
 
-) : BaseEntity() {
+    @ElementCollection
+    @CollectionTable(
+        name = "space_tags",
+        joinColumns = [JoinColumn(name = "space_id")]
+    )
+    @Column(name = "tag", nullable = false)
+    var tags: MutableList<String> = tags
+        protected set
 
     init {
         require(subImgUrls.isNotEmpty()) { "추가 사진은 비어있을 수 없습니다" }
@@ -64,15 +108,17 @@ class Space(
             category: SpaceCategory,
             name: String,
             mainImgUrl: String,
-            subImgUrls: List<String>,
+            subImgUrls: MutableList<String>,
             addressInfo: AddressInfo,
             amountInfo: AmountInfo,
             size: Double,
             operationalInfo: OperationalInfo,
             spaceModelUrl: String?,
+            modelThumbnailUrl: String?,
             user: User,
             phoneNumber: PhoneNumber,
-            precautions: String?
+            precautions: String?,
+            tags: MutableList<String> = mutableListOf(),
         ) = Space(
             spaceCategory = category,
             name = name,
@@ -83,14 +129,41 @@ class Space(
             size = size,
             operationalInfo = operationalInfo,
             spaceModelUrl = spaceModelUrl,
+            modelThumbnailUrl = modelThumbnailUrl,
             user = user,
             phoneNumber = phoneNumber,
-            precautions = precautions
+            precautions = precautions,
+            tags = tags
         )
     }
 
-    fun update() {
-
+    public fun update(
+        category: SpaceCategory,
+        name: String,
+        mainImgUrl: String,
+        subImgUrls: MutableList<String>,
+        addressInfo: AddressInfo,
+        amountInfo: AmountInfo,
+        size: Double,
+        operationalInfo: OperationalInfo,
+        spaceModelUrl: String?,
+        modelThumbnailUrl: String?,
+        phoneNumber: PhoneNumber,
+        precautions: String?,
+        tags: MutableList<String> = mutableListOf(),
+    ) {
+        this.spaceCategory   = category
+        this.name            = name
+        this.mainImgUrl      = mainImgUrl
+        this.subImgUrls      = subImgUrls
+        this.addressInfo     = addressInfo
+        this.amountInfo      = amountInfo
+        this.size            = size
+        this.operationalInfo = operationalInfo
+        this.spaceModelUrl   = spaceModelUrl
+        this.modelThumbnailUrl = modelThumbnailUrl
+        this.phoneNumber     = phoneNumber
+        this.precautions     = precautions
+        this.tags = tags
     }
-
 }
