ae0b83ff555f386d8d6a504eaef15d0fbbad209c
saeyeonn <sy01lim@naver.com>
2025-08-13 17:01:36 +0900

    feat(space): SpaceErrorCode 추가
    - NOT_FOUND 정의
      - 잘못된 공간 아이디 입력 시 NotFoundException 발생

--- src/main/kotlin/com/woonit/wonnit/global/exception/code/SpaceErrorCode.kt
+++ src/main/kotlin/com/woonit/wonnit/global/exception/code/SpaceErrorCode.kt
@@ -9,6 +9,7 @@ enum class SpaceErrorCode(
     override val type: String
 ) : ErrorCode {
 
+    /* VALIDATION 400 */
     INVALID_CATEGORY(
         "Space_001",
         "공간 카테고리가 유효하지 않습니다.",
@@ -22,5 +23,13 @@ enum class SpaceErrorCode(
         HttpStatus.BAD_REQUEST,
         "https://api.wonnit.com/errors/space/invalid-size"
     ),
+
+    /* NOT FOUND 404 */
+    NOT_FOUND(
+        "Space_003",
+        "공간을 찾을 수 없습니다.",
+        HttpStatus.NOT_FOUND,
+        "https://api.wonnit.com/errors/space/not-found"
+    )
 }
 
