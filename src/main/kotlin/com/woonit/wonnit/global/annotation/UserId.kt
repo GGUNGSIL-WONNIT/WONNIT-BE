c12dd227caf9aef79b38f8ad502f774f58cda091
maruhxn <maruhan1016@gmail.com>
2025-08-14 19:55:29 +0900

    feat: UserIdArgumentResolver 추가

---
+++ src/main/kotlin/com/woonit/wonnit/global/annotation/UserId.kt
@@ -0,0 +1,5 @@
+package com.woonit.wonnit.global.annotation
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class UserId
---
+++ src/main/kotlin/com/woonit/wonnit/global/config/WebConfig.kt
@@ -0,0 +1,15 @@
+package com.woonit.wonnit.global.config
+
+import com.woonit.wonnit.global.resolver.UserIdArgumentResolver
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig(
+    private val userIdArgumentResolver: UserIdArgumentResolver
+) : WebMvcConfigurer {
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(userIdArgumentResolver)
+    }
+}
