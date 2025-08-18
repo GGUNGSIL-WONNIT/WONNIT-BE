aca804d2b468bfbdb92f4ca1c76b4e86a72ecd76
saeyeonn <dev.saeyeon@gmail.com>
2025-08-18 19:19:58 +0900

    refactor: PresignUploadRequest 삭제 후 이미지명을 파라미터로 받도록 수정
    - 이미지 업로드를 위한 PresignedUrl 생성 요청 시 이미지명을 파라미터에 포함

--- src/main/kotlin/com/woonit/wonnit/domain/space/dto/PresignUploadRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.woonit.wonnit.domain.space.dto
-
-import io.swagger.v3.oas.annotations.media.Schema
-
-data class PresignUploadRequest(
-    @Schema(description = "이미지명")
-    val fileName: String
-)
