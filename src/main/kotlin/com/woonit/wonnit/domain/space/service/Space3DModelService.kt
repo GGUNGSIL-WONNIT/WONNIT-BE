5de320d885c89a6b1dee18c37ecc5fc461c7df09
saeyeonn <dev.saeyeon@gmail.com>
2025-08-18 20:23:42 +0900

    feat(space): 서비스 어노테이션 추가

--- src/main/kotlin/com/woonit/wonnit/domain/space/service/Space3DModelService.kt
+++ src/main/kotlin/com/woonit/wonnit/domain/space/service/Space3DModelService.kt
@@ -3,7 +3,9 @@ package com.woonit.wonnit.domain.space.service
 import com.woonit.wonnit.domain.space.dto.PresignUploadResponse
 import com.woonit.wonnit.infra.s3.S3PresignService
 import com.woonit.wonnit.infra.s3.S3Uploader
+import org.springframework.stereotype.Service
 
+@Service
 class Space3DModelService(
     private val s3PresignService: S3PresignService,
 ) {
--- src/main/kotlin/com/woonit/wonnit/domain/space/service/SpaceImageService.kt
+++ src/main/kotlin/com/woonit/wonnit/domain/space/service/SpaceImageService.kt
@@ -3,7 +3,9 @@ package com.woonit.wonnit.domain.space.service
 import com.woonit.wonnit.domain.space.dto.PresignUploadResponse
 import com.woonit.wonnit.infra.s3.S3PresignService
 import com.woonit.wonnit.infra.s3.S3Uploader
+import org.springframework.stereotype.Service
 
+@Service
 class SpaceImageService(
     private val s3PresignService: S3PresignService,
     private val s3Uploader: S3Uploader
