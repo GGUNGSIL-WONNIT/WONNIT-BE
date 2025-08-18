fda1042f55d2acb15e513422413acc1469c7638a
saeyeonn <dev.saeyeon@gmail.com>
2025-08-18 19:20:10 +0900

    docs: 스웨거 어노테이션 예시 수정

--- src/main/kotlin/com/woonit/wonnit/domain/space/dto/PresignUploadResponse.kt
+++ src/main/kotlin/com/woonit/wonnit/domain/space/dto/PresignUploadResponse.kt
@@ -6,6 +6,6 @@ data class PresignUploadResponse(
     @Schema(description = "presigned URL", example = "https://example.amazon.aws.com")
     val uploadUrl: String,
 
-    @Schema(description = "이미지 키 값", example = "uploads/028195e0-6999-137d-a747-0a02b343a12e/0289s3e0-9231-297d-a001-0a02ba2dd12e-ex")
+    @Schema(description = "이미지 키 값", example = "uploads/0289s3e0-9231-297d-a001-0a02ba2dd12e-ex")
     val key: String
 )
